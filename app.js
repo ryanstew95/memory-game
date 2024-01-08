d94c83e0cb904a0b51e36850489c9e41bc0ed1c7
Ryan Stewart <ryanstew17@gmail.com>
2024-01-08 13:40:29 -0400

    cards flip function

--- app.js
+++ app.js
@@ -52,13 +52,21 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid');
-
+const cardsChosen = []
 function createBoard () {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement('img')
     card.setAttribute('src', 'images/blank.png')
     card.setAttribute('data-id', i)
+    card.addEventListener('click', flipCard)
     gridDisplay.appendChild(card)
   }
 }
 createBoard()
+
+function flipCard() {
+  let cardId = this.getAttribute('data-id')
+  cardsChosen.push(cardArray[cardId].name)
+console.log(cardsChosen);
+this.setAttribute('src', cardArray[cardId].img)
+}
